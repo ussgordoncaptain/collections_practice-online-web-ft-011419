bf93cf00af61eda0bb3c7cf2d75138a859b7af25
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-19 04:34:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
-
+def sort_array_asc(arr)
+  final = arr.sort do |a,b|
+    a <=> b
+  end
+  return final
+end
+def sort_array_desc(arr)
+  final = arr.sort do |a,b|
+    (a <=> b)*-1
+  end
+  return final
+end
+def sort_array_char_count(arr)
+  final = arr.sort do |x, y|
+    x.length <=>y.length
+  end
+  return final
+end
+def swap_elements(arr)
+  eleA=arr[1]
+  eleB=arr[2]
+  arr[1] = eleB
+  arr[2] = eleA
+end
+def reverse_array(arr)
+  ii= arr.size - 1
+  final = []
+  while(ii>=0)
+  final << arr[ii]
+  ii -= 1
+  end 
+end
+def kesha_maker(arr)
+  final = []
+  arr.each do |word|
+    
+    new = word[0..1]+$+word[3..-1]
+    final << new
+  end
+  return final
+end 
+def find_a (arr)
+  final = []
+  arr.each do |x|
+    if x.start_with?("a")
+     x << final
+    end
+  end
+  return final
+end
+    
